99a66b5b04ef24f27c3f9ee983fd1226fcbe6695
InsaneMonster <pasqualini@diism.unisi.it>
2019-09-29 12:04:08 +0200

    Improved pack function, added unpack function, updated package __init__ accordingly

--- nistrng/__init__.py
+++ nistrng/__init__.py
@@ -15,4 +15,4 @@ from .test import Test, Result
 
 # Import functions
 
-from .functions import run_all_battery, run_by_name_battery, run_in_order_battery, check_eligibility_by_name_battery, check_eligibility_all_battery, pack_sequence, SP800_22R1A_BATTERY
+from .functions import run_all_battery, run_by_name_battery, run_in_order_battery, check_eligibility_by_name_battery, check_eligibility_all_battery, pack_sequence, unpack_sequence, SP800_22R1A_BATTERY
